19a38cbdda9a6a27d7bff3abdb2f2d91e5ed79e6
Taylor White <CSQ@qlik.com>
2018-08-06 15:48:17 -0700

    code for react router added

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactRouterExample from './ReactRouterExample'; 
+
 // import 'bootstrap/dist/css/bootstrap-grid.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<ReactRouterExample />, document.getElementById('root'));
