f773af9a60ccb76d3ac0186178ad2fb9ba34d1ca
Taylor White <CSQ@qlik.com>
2018-08-06 13:23:43 -0700

    adding material UI toolbar and card

--- src/client/App.js
+++ src/client/App.js
@@ -2,47 +2,49 @@ import React, { Component } from "react";
 import "./app.css";
 import ReactImage from "./react.png";
 import Button from '@material-ui/core/Button';
-
+import AppBar from './AppBar'; 
+import MediaCard from './MediaCard'; 
 
 class Matbutton extends Component {
   handleClick() {
-    alert('clicked!')
+	alert('clicked!')
   }
   render () {
-    return (
-      <Button variant="contained" color="primary" onClick={this.handleClick}>
-        Material UI Button
-      </Button>
-    );  
+	return (
+	  <Button variant="contained" color="primary" onClick={this.handleClick}>
+		Material UI Button
+	  </Button>
+	);  
   }   
 }
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { username: null };
+	super(props);
+	this.state = { username: null };
   }
 
   componentDidMount() {
-    fetch("/api/getUsername")
-      .then(res => res.json())
-      .then(user => this.setState({ username: user.username }));
+	fetch("/api/getUsername")
+	  .then(res => res.json())
+	  .then(user => this.setState({ username: user.username }));
   }
 
   render() {
-    return (
-      <div>
-        {this.state.username ? (
-          <h1>Hello {this.state.username}</h1>
-        ) : (
-          <h1>Loading.. please wait!</h1>
-        )}        
-        <Matbutton/>
-      </div>      
-    );
+	return (
+	  <div>
+		<AppBar/>		
+		{this.state.username ? (
+		  <h1>Hello {this.state.username}</h1>
+		) : (
+		  <h1>Loading.. please wait!</h1>
+		)}        
+		<Matbutton/>
+		<MediaCard/>
+	  </div>      
+	);
   }
 }
 
-console.log('App type', typeof(App)); 
 
 export default App; 
--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-console.log('keys App', Object.keys(App)); 
 
 ReactDOM.render(<App />, document.getElementById('root'));
